df308eec936f83b4acaf5a6722288d1411be0bc6
venkat-s <110343193+venkat-s@users.noreply.github.com>
2022-08-01 12:28:31 +0530

    Update ansible-infra.tf

--- ansible-infra.tf
+++ ansible-infra.tf
@@ -1,11 +1,11 @@
-#!AWS Provider
+#AWS Provider
 provider "aws" {
   region = "us-east-1"
   access_key = "AKIAYMLONH22QX757PJV"
   secret_key = "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"
 }
 
-!Ansible Master
+#Ansible Master
 resource "aws_instance" "ansible_master" { 
   ami           = "ami-02358d9f5245918a3"
   instance_type = "t2.micro"
@@ -16,7 +16,7 @@ resource "aws_instance" "ansible_master" {
   }
 }
 
-!Ansible Node1
+#Ansible Node1
 resource "aws_instance" "ansible_node1" {
   ami           = "ami-02358d9f5245918a3"
   instance_type = "t2.micro"
@@ -26,7 +26,7 @@ resource "aws_instance" "ansible_node1" {
   }
 }
 
-!Ansible Node2
+#Ansible Node2
 resource "aws_instance" "ansible_node2" {
   ami           = "ami-02358d9f5245918a3"
   instance_type = "t2.micro"
