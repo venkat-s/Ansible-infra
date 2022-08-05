b0702b01cae17d63aa3f76f3de35dd63b820fa6e
venkat-s <110343193+venkat-s@users.noreply.github.com>
2022-08-05 18:34:10 +0530

    2 lines updated

--- ansible-infra.tf
+++ ansible-infra.tf
@@ -1,8 +1,8 @@
 #AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAYMLONH22QX757PJV"
-  secret_key = "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"
+  access_key = "AWS_ACCESS_KEY"
+  secret_key = "AWS_SECRET-KEY"
 }
 
 #Ansible Master
